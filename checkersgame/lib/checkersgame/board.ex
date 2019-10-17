f2243674a5a3cdc6387769ea9a6f85b98390d020
marielle1022 <55012214+marielle1022@users.noreply.github.com>
2019-10-17 19:56:24 -0400

    update board

--- checkersgame/lib/checkersgame/board.ex
+++ checkersgame/lib/checkersgame/board.ex
@@ -1,2 +1,5 @@
 defmodule Checkersgame.Board do
+  #NB: Is this alias only usable within this module?
+  #Question: Should Piece module be nexted within this?
+  alias Checkersgame.Board, as: Board
 end

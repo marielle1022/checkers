69084d39a684be819b921753f534753f28394bb8
marielle1022 <marielle.riveros@gmail.com>
2019-10-17 20:29:04 -0400

    asdfasdf

--- checkersgame/lib/checkersgame/board.ex
+++ checkersgame/lib/checkersgame/board.ex
@@ -1,11 +1,15 @@
 defmodule Checkersgame.Board do
-  #Note: Is this alias only usable within this module?
-  #Question: Should Piece module be nexted within this?
+  # Note: Is this alias only usable within this module?
+  # Question: Should Piece module be nexted within this?
   alias Checkersgame.Board, as: Board
 
-  def new do {
+  def new do
     %{
       # set up matrix
-      }
-   end
+    }
+  end
+
+  def create_board do
+    # Matrix.new()
+  end
 end

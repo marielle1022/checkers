2518d3250f0fc8ea3106be09c89b6b889dd5da32
marielle1022 <marielle.riveros@gmail.com>
2019-10-17 21:33:25 -0400

    board matrix, updating functions created

--- checkersgame/lib/checkersgame/board.ex
+++ checkersgame/lib/checkersgame/board.ex
@@ -3,13 +3,40 @@ defmodule Checkersgame.Board do
   # Question: Should Piece module be nexted within this?
   alias Checkersgame.Board, as: Board
 
+  defstruct
+
   def new do
     %{
       # set up matrix
+      # Question: how to define this so it can be updated at each move? Is it possible?
+      board: create_board()
     }
   end
 
+  # To create nested map, used
+  # https://blog.danielberkompas.com/2016/04/23/multidimensional-arrays-in-elixir/
   def create_board do
-    # Matrix.new()
+    # Dark squares (the only ones being used) will be represented with 0
+    # Light squares (which won't be used) will be represented with -1
+    starting_board = %{
+      0 => %{0 => 0, 1 => -1, 2 => 0, 3 => -1, 4 => 0, 5 => -1, 6 => 0, 7 => -1, 8 => 0, 9 => -1},
+      1 => %{0 => -1, 1 => 0, 2 => -1, 3 => 0, 4 => -1, 5 => 0, 6 => -1, 7 => 0, 8 => -1, 9 => 0},
+      2 => %{0 => 0, 1 => -1, 2 => 0, 3 => -1, 4 => 0, 5 => -1, 6 => 0, 7 => -1, 8 => 0, 9 => -1},
+      3 => %{0 => -1, 1 => 0, 2 => -1, 3 => 0, 4 => -1, 5 => 0, 6 => -1, 7 => 0, 8 => -1, 9 => 0},
+      4 => %{0 => 0, 1 => -1, 2 => 0, 3 => -1, 4 => 0, 5 => -1, 6 => 0, 7 => -1, 8 => 0, 9 => -1},
+      5 => %{0 => -1, 1 => 0, 2 => -1, 3 => 0, 4 => -1, 5 => 0, 6 => -1, 7 => 0, 8 => -1, 9 => 0},
+      6 => %{0 => 0, 1 => -1, 2 => 0, 3 => -1, 4 => 0, 5 => -1, 6 => 0, 7 => -1, 8 => 0, 9 => -1},
+      7 => %{0 => -1, 1 => 0, 2 => -1, 3 => 0, 4 => -1, 5 => 0, 6 => -1, 7 => 0, 8 => -1, 9 => 0},
+      8 => %{0 => 0, 1 => -1, 2 => 0, 3 => -1, 4 => 0, 5 => -1, 6 => 0, 7 => -1, 8 => 0, 9 => -1},
+      9 => %{0 => -1, 1 => 0, 2 => -1, 3 => 0, 4 => -1, 5 => 0, 6 => -1, 7 => 0, 8 => -1, 9 => 0}
+    }
+  end
+
+  # x: x coordinate of board matrix
+  # y: y coordinate of board matrix
+  # value: value to be put in board matrix at coordinates x, y
+  # Note: works in iex if pass board as parameter
+  def update_board(x, y, value) do
+    board = put_in(board[x][y], value)
   end
 end

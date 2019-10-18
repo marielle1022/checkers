4dc9779edf2b41990cfccc371cc8297e007691eb
marielle1022 <55012214+marielle1022@users.noreply.github.com>
2019-10-17 20:00:31 -0400

    new function in board

--- checkersgame/lib/checkersgame/board.ex
+++ checkersgame/lib/checkersgame/board.ex
@@ -2,4 +2,10 @@ defmodule Checkersgame.Board do
   #NB: Is this alias only usable within this module?
   #Question: Should Piece module be nexted within this?
   alias Checkersgame.Board, as: Board
+  
+  def new do {
+    %{
+      # set up matrix
+      }
+   end
 end

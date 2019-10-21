73178caa1945079150de03e38b485a5f76dafa35
Kevin Gendron <kevin.gendron182@gmail.com>
2019-10-21 19:58:09 -0400

    Added token stuff

--- checkersgame/lib/checkersgame_web/router.ex
+++ checkersgame/lib/checkersgame_web/router.ex
@@ -7,6 +7,17 @@ defmodule CheckersgameWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug MyAuth
+    plug :put_user_token
+  end
+    //
+  defp put_user_token(conn, _) do
+    if current_user = conn.assigns[:current_user] do
+      token = Phoenix.Token.sign(conn, "user socket", current_user.id)
+      assign(conn, :user_token, token)
+    else
+      conn
+    end
   end
 
   pipeline :api do
@@ -21,6 +32,8 @@ defmodule CheckersgameWeb.Router do
     get "/game/:name", PageController, :game
 
   end
+
+  
 
   # Other scopes may use custom stacks.
   # scope "/api", CheckersgameWeb do

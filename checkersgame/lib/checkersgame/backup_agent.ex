2e4bf5fe2b8414209a435e204a20fabbef5bd38a
Kevin Gendron <kevin.gendron182@gmail.com>
2019-10-22 23:55:10 -0400

    added gamesup and backupagent

---
+++ checkersgame/lib/checkersgame/backup_agent.ex
@@ -0,0 +1,22 @@
+defmodule Checkersgame.BackupAgent do
+    use Agent
+  
+    # This is basically just a global mutable map.
+    # Using this from the hangman backup_agent.ex file
+
+    def start_link(_args) do
+      Agent.start_link(fn -> %{} end, name: __MODULE__)
+    end
+  
+    def put(name, val) do
+      Agent.update __MODULE__, fn state ->
+        Map.put(state, name, val)
+      end
+    end
+  
+    def get(name) do
+      Agent.get __MODULE__, fn state ->
+        Map.get(state, name)
+      end
+    end
+  end
---
+++ checkersgame/lib/checkersgame/game_sup.ex
@@ -0,0 +1,21 @@
+  
+defmodule Checkersgame.GameSup do
+    use DynamicSupervisor
+  
+    # Assuming this is nearly a template, referencing 
+    # it from the hangman backup_agent.ex file 
+    
+    def start_link(arg) do
+      DynamicSupervisor.start_link(__MODULE__, arg, name: __MODULE__)
+    end
+  
+    @impl true
+    def init(_arg) do
+      {:ok, _} = Registry.start_link(keys: :unique, name: Checkersgame.GameReg)
+      DynamicSupervisor.init(strategy: :one_for_one)
+    end
+  
+    def start_child(spec) do
+      DynamicSupervisor.start_child(__MODULE__, spec)
+    end
+  end

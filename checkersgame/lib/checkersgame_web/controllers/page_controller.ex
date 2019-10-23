a8f916dfe7b367f10c410c51e026e3995af1f460
marielle1022 <marielle.riveros@gmail.com>
2019-10-23 17:10:30 -0400

    game name showing up

--- checkersgame/lib/checkersgame_web/controllers/page_controller.ex
+++ checkersgame/lib/checkersgame_web/controllers/page_controller.ex
@@ -1,20 +1,23 @@
 defmodule CheckersgameWeb.PageController do
   use CheckersgameWeb, :controller
-  
+
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
   end
 
   def join(conn, %{"join" => %{"user" => user, "game" => game}}) do
     conn
     |> put_session(:user, user)
-    |> redirect(to: "/game/#{game}")
+    |> put_session(:game, game)
+    |> redirect(to: "/game/#{user}")
   end
 
-  def game(conn, params) do
+  def game(conn) do
     user = get_session(conn, :user)
+    game = get_session(conn, :game)
+
     if user do
-      render conn, "game.html", game: params["game"], user: user
+      render(conn, "game.html", game: game, user: user)
     else
       conn
       |> put_flash(:error, "Must pick a username")
